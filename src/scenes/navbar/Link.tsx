e60395e530532226bb0139033c02d91376b81c7a
lahiruR <lahiru.rathnasheela@synapsys.lk>
2025-03-23 14:04:50 +0530

    Nav bar complete

---
+++ src/scenes/navbar/Link.tsx
@@ -0,0 +1,25 @@
+import { SelectedPage } from "@/shared/types";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+
+type Props = {
+  page: string;
+  selectedPage: SelectedPage;
+  setSelectedPage: (value: SelectedPage) => void;
+};
+
+const Link = ({ page, selectedPage, setSelectedPage }: Props) => {
+  const lowercasePage = page.toLowerCase().replace(/ /g, "") as SelectedPage;
+  return (
+    <AnchorLink
+      className={`${
+        selectedPage === lowercasePage ? "text-primary-500" : ""
+      } transition duration-500 hover:text-primary-300 `}
+      href={`#${lowercasePage}`}
+      onClick={() => setSelectedPage(lowercasePage)}
+    >
+      {page}
+    </AnchorLink>
+  );
+};
+
+export default Link;
